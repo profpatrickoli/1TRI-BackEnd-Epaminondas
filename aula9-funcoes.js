706725f8b5886965bec98cfce7aa77749c34b4f0
Patrick <patrick.staroin@escola.pr.gov.br>
2025-03-24 08:47:37 -0300

    Correção atividade

--- aula9-funcoes.js
+++ aula9-funcoes.js
@@ -15,5 +15,35 @@ function verificarCNH(idade, possuiCNH){
         console.log("Não pode")
     }
 }
-
 verificarCNH(27, true);
+
+
+const lanches = ["x-egg", "x-ovo", "x-bacon", "x-tudo", "x-frango"];
+const precos = [12, 15, 15, 20, 12];
+
+function imprimeCardapio(lanches, precos){
+    const tamanho = lanches.length;
+    let contador = 0;
+    // Imprimam o cardápio usando o laço While
+    while(contador < tamanho){
+        console.log(lanches[contador] + " - R$ " + precos[contador]);
+        contador = contador + 1;
+    }
+}
+
+function calculaDesconto(){
+    let precosDesconto = [];
+    const tamanho = precos.length;
+    contador = 0;
+    while(contador < tamanho){
+        precosDesconto[contador] = precos[contador] - (precos[contador] * 10/100);
+        contador = contador + 1;
+    }
+    return precosDesconto;
+}
+
+let precosComDesconto = calculaDesconto();
+
+imprimeCardapio(lanches, precos);
+calculaDesconto();
+imprimeCardapio(lanches, precosComDesconto);
